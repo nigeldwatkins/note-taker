2f9e48a4edb3fc7ef7bf4ef319d46081b3b2b72b
Nigel D Watkins <Nigeldwatkins@gmail.com>
2023-06-18 12:54:37 -0400

    setting up my api route file to get and post request

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,2 +1,19 @@
 // Importing packages/modules that will be needed for my code
 const router = require('express').Router();
+const fs = require("fs");
+
+// get request to the api notes endpoint of a server
+router.get('/api/notes', (req, res) => {
+const dbJson = JSON.parse(fs.readFileSync("db/db.json", "utf8"))
+res.json(dbJson) 
+}); 
+
+// route handler that handles a post request to api notes endpoint
+router.post('/api/notes', (req, res) => {
+const dbJson = JSON.parse(fs.readFileSync("db/db.json", "utf8"))
+dbJson.push(req.body)
+fs.writeFileSync("db/db.json",JSON.stringify(dbJson))
+res.json(dbJson)
+});
+
+module.exports = router;
