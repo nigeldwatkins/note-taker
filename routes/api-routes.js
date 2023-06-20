93f82d560f15f6ed6ed234d0f06e071c574ca561
Nigel D Watkins <Nigeldwatkins@gmail.com>
2023-06-19 23:02:02 -0400

    working with my api and html route files

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,19 +1,25 @@
 // Importing packages/modules that will be needed for my code
 const router = require('express').Router();
 const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
 // get request to the api notes endpoint of a server
-router.get('/api/notes', (req, res) => {
-const dbJson = JSON.parse(fs.readFileSync("db/db.json", "utf8"))
-res.json(dbJson) 
+router.get('/api/notes', async (req, res) => {
+const dbJson = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+res.json(dbJson); 
 }); 
 
 // route handler that handles a post request to api notes endpoint
 router.post('/api/notes', (req, res) => {
-const dbJson = JSON.parse(fs.readFileSync("db/db.json", "utf8"))
-dbJson.push(req.body)
-fs.writeFileSync("db/db.json",JSON.stringify(dbJson))
-res.json(dbJson)
+const dbJson = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+const newFeedback = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4(),
+};
+dbJson.push(newFeedback);
+fs.writeFileSync("db/db.json",JSON.stringify(dbJson));
+res.json(dbJson);
 });
 
 module.exports = router;
