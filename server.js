396ee3fb5fe52b2ca01fd6c3ffe486795db3b14c
Nigel D Watkins <Nigeldwatkins@gmail.com>
2023-06-19 23:38:55 -0400

    fixing a few mistakes

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // the routes to my html and api files
-const api_routes = require('./api-routes');
-const html_routes = require('./html-routes');
+const api_routes = require('./routes/api-routes');
+const html_routes = require('./routes/html-routes');
 
 // defines what my port will be 
-const Port = process.env.PORT || 5001;
+const Port = process.env.PORT || 3001;
 
 // setting up middleware in my express server
 app.use(express.urlencoded({ extended: false}));
@@ -16,6 +16,6 @@ app.use(express.static("public"));
 app.use(html_routes)
 app.use(api_routes)
 
-app.listen(PORT, () => {
-    console.log(`Server running on https://localhost:${PORT}`);
+app.listen(Port, () => {
+    console.log(`Server running on https://localhost:${Port}`);
 })
