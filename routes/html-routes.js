80bf6d1375eb79c1b2deaa26989814c8a2298021
Nigel D. Watkins <nigeldwatkins@gmail.com>
2023-06-20 20:44:22 -0400

    trying to re file starter code

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,12 +2,12 @@
 const router = require('express').Router();
 const path = require('path');
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './develop/public/index.html'))
+router.get('/', (req, res,) => {
+    res.sendFile(path.join(__filename, '../public/index.html'));
 });
 
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './develop/public/notes.html'))
+router.get('/notes', (req, res,) => {
+    res.sendFile(path.join(__filename, '../public/notes.html'))
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.use(html_routes)
 app.use(api_routes)
 
 app.listen(PORT, () => {
-    console.log(`Server running on https://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 })
